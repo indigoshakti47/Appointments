6088748283dad3bde1955d092158bff7900c831f
Indigo <indigoshakti47@unseen.is>
2021-01-26 21:11:22 -0500

    Listing the basic structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React, {Fragment} from "react";
+import React, {Fragment , useState} from "react";
 import Form from './components/Form'
+import Appointment from './components/Appointment'
 
 function App() {
+  const [appointments, saveAppointments] = useState([])
+  const createAppointment = appointment => {
+    saveAppointments([ ...appointments, appointment])
+  }
   return (
     <Fragment>
       <h1>Administrador de pacientes</h1>
       <div className="container">
         <div className="row">
           <div className="one-half column">
-            <Form/>
+            <Form
+            createAppointment={createAppointment}/>
           </div>
           <div className="one-half column">
-            2
+            <h2>List them all!</h2>
+            {appointments.map(appointment => (
+              <Appointment
+              key={appointment.id}
+              appointment={appointment}/>
+            ))}
           </div>
         </div>
       </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,43 +1,57 @@
-import React, { Fragment , useState } from "react";
-import uuid from 'uuid/v4'
+import React, { Fragment, useState } from "react";
+import { v4 as uuid } from 'uuid';
 
-const Form = () => {
-    const [appointment, updateAppointment] = useState({
-        pet:'',
-        owner:'',
-        date:'',
-        time:'',
-        symptoms:''
+const Form = ({ createAppointment }) => {
+  const [appointment, updateAppointment] = useState({
+    pet: "",
+    owner: "",
+    date: "",
+    time: "",
+    symptoms: "",
+  });
 
+  const [error, updateError] = useState(false);
+  const { pet, owner, date, time, symptoms } = appointment;
+
+  const updateState = (e) => {
+    updateAppointment({
+      ...appointment,
+      [e.target.name]: e.target.value,
     });
-
-    const [error, updateError] = useState (false)
-
-    const updateState = e =>{
-        updateAppointment({
-            ...appointment,
-            [e.target.name]: e.target.value
-        })
+  };
+  const submitAppointment = (e) => {
+    e.preventDefault();
+    if (
+      pet.trim() === "" ||
+      owner.trim() === "" ||
+      date.trim() === "" ||
+      time.trim() === "" ||
+      symptoms.trim() === ""
+    ) {
+      updateError(true);
+      return;
     }
-    const submitAppointment = e => {
-        e.preventDefault();
-        if (pet.trim()==='' || owner.trim()==='' || date.trim()==='' || time.trim()==='' || symptoms.trim()===''){
-            updateError(true)
-            return;
-        }
-        updateError(false)
-        appointment.id = uuid();
-        console.log(appointment)
+    updateError(false);
+    appointment.id = uuid();
+    createAppointment(appointment);
+    updateAppointment({
+      pet: "",
+      owner: "",
+      date: "",
+      time: "",
+      symptoms: "",
+    });
+  };
 
-    }
-
-    const {pet, owner, date, time, symptoms} = appointment;
   return (
     <Fragment>
       <h2>Create appointment</h2>
-      {error ? <p className="alert-error">All the fields are mandatory, please fill them out!</p> : null}
-      <form
-      onSubmit={submitAppointment}>
+      {error ? (
+        <p className="alert-error">
+          All the fields are mandatory, please fill them out!
+        </p>
+      ) : null}
+      <form onSubmit={submitAppointment}>
         <label>Pet's name</label>
         <input
           type="text"
@@ -78,20 +92,15 @@ const Form = () => {
 
         <label>Symptoms</label>
         <textarea
-        name="symptoms"
-        className="u-full-width"
-        onChange={updateState}
-        value={symptoms}>
-        
+          name="symptoms"
+          className="u-full-width"
+          onChange={updateState}
+          value={symptoms}
+        ></textarea>
 
-        </textarea>
-
-        <button
-        type="submit"
-        className="u-full-width button-primary">
-            CREATE
+        <button type="submit" className="u-full-width button-primary">
+          CREATE
         </button>
-
       </form>
     </Fragment>
   );
